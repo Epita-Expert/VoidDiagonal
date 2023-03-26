6dc10a56d4911f2d1137b47694d63319c2aed895
Nathan Podesta <nathan.podesta@icloud.com>
2023-03-26 13:24:11 +0200

    add table name

--- script-insert.py
+++ script-insert.py
@@ -1,8 +1,10 @@
 import json
 import psycopg2
+# pip install psycopg2
+# docker exec -it 4bacb14e172c psql -U myuser -d mydatabase --password
 
 # Load the JSON data from file
-with open('./medecins-output.json', 'r') as f:
+with open('./medecins.json', 'r') as f:
     data = json.load(f)
 
 # Connect to the PostgreSQL database
@@ -17,7 +19,7 @@ cur = conn.cursor()
 
 # Define the SQL query to insert a row
 query = '''
-    INSERT INTO your_table (gender, job, lon, lat, city, name, department, region) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+    INSERT INTO medecin (gender, job, lon, lat, city, name, department, region) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
 '''
 
 # Execute the SQL query for each row of data
